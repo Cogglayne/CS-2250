bf00d1a2675dcac0658a7a7350ae547b523c2705
Jordan <jordan41496@gmail.com>
2023-01-27 12:33:17 -0700

    finished challenge 3

--- Challenge 3/main/main.cpp
+++ Challenge 3/main/main.cpp
@@ -3,7 +3,29 @@
 
 #include <iostream>
 using namespace std;
+int promptForInteger();
+
 int main()
 {
-    
+    int totalSum = 0;
+    int input = 0;
+    bool isPositive;
+    do 
+    {
+       input = promptForInteger();
+       isPositive = input >= 0;
+       if (isPositive) 
+       {
+           totalSum += input;
+       }
+    } while (isPositive);
+    cout << "Total Sum: " << totalSum << endl;
+    return 0;
+}
+
+int promptForInteger() {
+    int input = 0;
+    cout << "Please enter an integer:" << endl;
+    cin >> input;
+    return input;
 }
