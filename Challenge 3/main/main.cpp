913bdd126b6e879d6b8e7efc8d4c02d80314155b
Jordan Arms <Jordan41496@gmail.com>
2023-01-27 15:53:52 -0700

    Formatted code and added documentation

--- Challenge 3/main/main.cpp
+++ Challenge 3/main/main.cpp
@@ -1,31 +1,39 @@
-// main.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
 using namespace std;
+
 int promptForInteger();
 
+/*
+* Program execution begins and ends here.
+* Sums numbers until a negative number is inputed
+* afterwords the total sum is outputed
+*/
 int main()
 {
-    int totalSum = 0;
-    int input = 0;
-    bool isPositive;
-    do 
-    {
-       input = promptForInteger();
-       isPositive = input >= 0;
-       if (isPositive) 
-       {
-           totalSum += input;
-       }
-    } while (isPositive);
-    cout << "Total Sum: " << totalSum << endl;
-    return 0;
+	int totalSum = 0;
+	int input = 0;
+	bool isPositive;
+	do
+	{
+		input = promptForInteger();
+		isPositive = input >= 0;
+		if (isPositive)
+		{
+			totalSum += input;
+		}
+	} while (isPositive);
+	cout << "Total Sum: " << totalSum << endl;
+	return 0;
 }
 
-int promptForInteger() {
-    int input = 0;
-    cout << "Please enter an integer:" << endl;
-    cin >> input;
-    return input;
+/*
+* Prompts for a number and then returns that number
+*/
+int promptForInteger()
+{
+	int input = 0;
+	cout << "Please enter an integer:" << endl;
+	cin >> input;
+	return input;
 }
