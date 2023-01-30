faae0680169cb4e237bf612b13aa6a89d42bceec
Jordan Arms <Jordan41496@gmail.com>
2023-01-29 22:00:00 -0700

    Made style changes to challenge 3

--- Challenge 3/main/main.cpp
+++ Challenge 3/main/main.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 using namespace std;
 
@@ -11,29 +10,40 @@ int promptForInteger();
 */
 int main()
 {
+	// variable creation
 	int totalSum = 0;
 	int input = 0;
 	bool isPositive;
+
+	// the loop continues until a negative number is inputed
 	do
 	{
+		// gets a number from the user
 		input = promptForInteger();
-		isPositive = input >= 0;
+
+		// checks if input is positive 
+		isPositive = (input >= 0);
+
+		// the number is added to total sum if input is positive
 		if (isPositive)
 		{
 			totalSum += input;
 		}
 	} while (isPositive);
+
 	cout << "Total Sum: " << totalSum << endl;
+
 	return 0;
 }
 
 /*
-* Prompts for a number and then returns that number
+* Gets a number from the user and then returns that number
 */
 int promptForInteger()
 {
 	int input = 0;
 	cout << "Please enter an integer:" << endl;
 	cin >> input;
+
 	return input;
 }
